ee0dbf9233aac99518832351b5507697081bb26e
Nikhil Punse <nikhil.punse5@gmail.com>
2023-02-15 14:53:17 +0530

    login signup page added

--- src/App.js
+++ src/App.js
@@ -5,6 +5,8 @@ import Home from './components/Home'
 import Collection from './components/Collection'
 import ProductDetails from './components/ProductDetails'
 import Header from './components/Header'
+import Login from './components/Login'
+import Signup from './components/Signup'
 
 const App = () => {
   return (
@@ -14,6 +16,8 @@ const App = () => {
       <Route path='/' element={<Home/>}></Route>
       <Route path='/collection' element={<Collection/>}></Route>
       <Route path='/product' element={<ProductDetails/>}></Route>
+      <Route path='/login' element={<Login/>}></Route>
+      <Route path='/signup' element={<Signup/>}></Route>
     </Routes>
     
   </>
